63f91abd18d2c8d942a49d3061761b57e2874c66
FBIATOMIC <165179111+FBIATOMIC@users.noreply.github.com>
2025-01-11 23:05:41 +0600

    index.js

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
-const
-  { Client } = require('discord.js-selfbot-v13'),
-  client = new Client({checkUpdate: false,}),
-  reloadPresence = require("./config.js"),
-  keepAlive = require('./keep_alive.js');
+const { Client } = require('discord.js-selfbot-v13');
+const client = new Client({ checkUpdate: false });
+const reloadPresence = require('./config.js');
+const keepAlive = require('./keep_alive.js');
 
+// Keep the bot alive
 keepAlive();
 
 if (!process.env.TOKEN) {
-  console.error("Add a token inside Secrets.");
-  process.exit();
+    console.error("Add a token inside Secrets.");
+    process.exit();
 }
 
 client.login(process.env.TOKEN);
 
-  console.clear();
-  client.on("ready", async () => {
-        global.startTime = new Date();
-        reloadPresence(client);
-    })
+console.clear();
+client.on('ready', async () => {
+    global.startTime = new Date();
+    reloadPresence(client);
+    console.log('Discord RPC is running!');
+});
