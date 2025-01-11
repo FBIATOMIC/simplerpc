ee0cb7f569a1c3cb2005b47e4c1575f4f8c587dd
FBIATOMIC <165179111+FBIATOMIC@users.noreply.github.com>
2025-01-11 22:30:59 +0600

    config.js

--- config.js
+++ config.js
@@ -13,8 +13,6 @@ function reloadPresence(client) {
         .setStartTimestamp(global.startTime)
         .setAssetsLargeImage('https://media.discordapp.net/attachments/1301227546251366424/1302578046599041024/0704.gif?ex=67839757&is=678245d7&hm=27ee8e9cd3f55f178cddd06fef5686b8d088f227b8d567a8cda5ae8d79008c14&=') //You can put links in tenor or discord and etc.
         .setAssetsLargeText('⚠ Classified as a national Threat ⚠') //Text when you hover the Large image
-        .setAssetsSmallImage('Small Image URL') //You can put links in tenor or discord and etc.
-        .setAssetsSmallText('Small Text') //Text when you hover the Small image
         .addButton('♠ "Illegal Video" ♠', 'https://www.youtube.com/watch?v=ovT5AcJFaMQ')
         .addButton('⚠ "[YouTube Playlist]" ⚠', 'https://youtube.com/playlist?list=PLQBUreVNNjUgKhBL0eM-2Ak66xEuvsPAj&si=813wdMtDkjhK_pZb');
     client.user.setActivity(activity.toJSON())
