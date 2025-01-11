6cf7d5634a462fc6229f7e75e959326ee4aacbd2
FBIATOMIC <165179111+FBIATOMIC@users.noreply.github.com>
2025-01-11 18:18:40 +0600

    config.js

--- config.js
+++ config.js
@@ -4,21 +4,20 @@ const
 function reloadPresence(client) {
     const activity = new discord.RichPresence()
       //more command https://discordjs-self-v13.netlify.app/#/docs/docs/main/class/RichPresence or https://github.com/aiko-chan-ai/discord.js-selfbot-v13/blob/main/Document/RichPresence.md
-    .setApplicationId('1')
-    .setType('STREAMING')
-    .setURL('https://www.youtube.com/watch?v=ovT5AcJFaMQ') //Must be a youtube video link 
-    .setState('☣ [King of Hackers] ☣')
-    .setName('⚠◥◣_◢◤ "HACKER" ◥◣_◢◤⚠')
-    .setDetails(`💢 "ATOMIC-Tier Hacker" 💢`)
-    .setStartTimestamp(Date.now())
- .setAssetsLargeImage('https://imgur.com/uLmY6Lz.gif') //You can put links in tenor or discord and etc.
-    .setAssetsLargeText('⚠ Classified as a national Threat ⚠') //Text when you hover the Large image
-    .setAssetsSmallImage('Small Image URL') //You can put links in tenor or discord and etc.
-    .setAssetsSmallText('Small Text') //Text when you hover the Small image
-    .addButton('♠ "Illegal Video" ♠', 'https://www.youtube.com/watch?v=ovT5AcJFaMQ')
-    .addButton('⚠ "[YouTube Playlist]" ⚠', 'https://youtube.com/playlist?list=PLQBUreVNNjUgKhBL0eM-2Ak66xEuvsPAj&si=813wdMtDkjhK_pZb');
-
-  client.user.setActivity(r);
-  client.user.setPresence({ status: "idle" }); //dnd, online, idle, offline
+        .setApplicationId('1')
+        .setType('STREAMING')
+        .setURL('https://www.youtube.com/watch?v=ovT5AcJFaMQ') //Must be a youtube video link 
+        .setState('☣ [King of Hackers] ☣')
+        .setName('⚠◥◣_◢◤ "HACKER" ◥◣_◢◤⚠')
+        .setDetails(`💢 "ATOMIC-Tier Hacker" 💢`)
+        .setStartTimestamp(global.startTime)
+        .setAssetsLargeImage('https://imgur.com/uLmY6Lz') //You can put links in tenor or discord and etc.
+        .setAssetsLargeText('⚠ Classified as a national Threat ⚠') //Text when you hover the Large image
+        .setAssetsSmallImage('Small Image URL') //You can put links in tenor or discord and etc.
+        .setAssetsSmallText('Small Text') //Text when you hover the Small image
+        .addButton('♠ "Illegal Video" ♠', 'https://www.youtube.com/watch?v=ovT5AcJFaMQ')
+        .addButton('⚠ "[YouTube Playlist]" ⚠', 'https://youtube.com/playlist?list=PLQBUreVNNjUgKhBL0eM-2Ak66xEuvsPAj&si=813wdMtDkjhK_pZb');
+    client.user.setActivity(activity.toJSON())
+    client.user.setStatus("idle")
 };
 module.exports = reloadPresence;
