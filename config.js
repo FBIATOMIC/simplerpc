50ccf7357fd63df579ad35aa4217325a11a8584f
FBIATOMIC <165179111+FBIATOMIC@users.noreply.github.com>
2025-01-12 16:41:57 +0600

    config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
-const
-  discord = require("discord.js-selfbot-v13");
- 
+const discord = require("discord.js-selfbot-v13");
+
 function reloadPresence(client) {
     const activity = new discord.RichPresence()
         .setApplicationId('1')
         .setType('STREAMING')
-        .setURL('https://www.youtube.com/watch?v=ovT5AcJFaMQ') //Must be a youtube video link 
+        .setURL('https://www.youtube.com/watch?v=ovT5AcJFaMQ') // Must be a YouTube video link
         .setState('☣ [King of Hackers] ☣')
         .setName('⚠◥◣_◢◤ "HACKER" ◥◣_◢◤⚠')
-        .setDetails(💢 "ATOMIC-Tier Hacker" 💢)
+        .setDetails('💢 ATOMIC-Tier Hacker 💢') // Corrected syntax
         .setStartTimestamp(global.startTime)
-        .setAssetsLargeImage('https://i.imgur.com/uLmY6Lz.gif') //You can put links in tenor or discord and etc.
-        .setAssetsLargeText('⚠ Classified as a national Threat ⚠') //Text when you hover the Large image
-        .addButton('♠ "Illegal Video" ♠', 'https://www.youtube.com/watch?v=ovT5AcJFaMQ')
-        .addButton('⚠ "[YouTube Playlist]" ⚠', 'https://youtube.com/playlist?list=PLQBUreVNNjUgKhBL0eM-2Ak66xEuvsPAj&si=813wdMtDkjhK_pZb');
-    client.user.setActivity(activity.toJSON())
-    client.user.setStatus("idle")
-};
+        .setAssetsLargeImage('https://i.imgur.com/uLmY6Lz.gif') // Image link
+        .setAssetsLargeText('⚠ Classified as a national Threat ⚠') // Hover text for the large image
+        .addButton('♠ "Illegal Video" ♠', 'https://www.youtube.com/watch?v=ovT5AcJFaMQ');
+    
+    client.user.setActivity(activity.toJSON());
+    client.user.setStatus("idle");
+}
+
 module.exports = reloadPresence;
