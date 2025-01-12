24eb531567ed07f9bd7a1a08fd9a7072b038b68c
FBIATOMIC <165179111+FBIATOMIC@users.noreply.github.com>
2025-01-12 17:00:10 +0600

    config.js

--- config.js
+++ config.js
@@ -8,9 +8,15 @@ function reloadPresence(client) {
         .setState('☣ [King of Hackers] ☣')
         .setName('⚠◥◣_◢◤ "HACKER" ◥◣_◢◤⚠')
         .setDetails('💢 ATOMIC-Tier Hacker 💢') // Corrected syntax
-        .setStartTimestamp(global.startTime)
-        .setAssetsLargeImage('https://i.imgur.com/uLmY6Lz.gif') // Image link
-        .setAssetsLargeText('⚠ Classified as a national Threat ⚠') // Hover text for the large image
+        .setStartTimestamp(global.startTime);
+
+    try {
+        activity.setAssetsLargeImage('https://i.imgur.com/uLmY6Lz.gif'); // Image link
+    } catch (error) {
+        console.error("Invalid URL for large image:", error.message);
+    }
+
+    activity.setAssetsLargeText('⚠ Classified as a national Threat ⚠') // Hover text for the large image
         .addButton('♠ "Illegal Video" ♠', 'https://www.youtube.com/watch?v=ovT5AcJFaMQ');
     
     client.user.setActivity(activity.toJSON());
