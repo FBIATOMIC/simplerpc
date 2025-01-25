41dca83cf9e5251fa2b2a87e7bbbb535b6eb53c5
FBIATOMIC <165179111+FBIATOMIC@users.noreply.github.com>
2025-01-25 17:01:44 +0600

    config.js

--- config.js
+++ config.js
@@ -4,19 +4,12 @@ function reloadPresence(client) {
     const activity = new discord.RichPresence()
         .setApplicationId('1')
         .setType('STREAMING')
-        .setURL('https://www.youtube.com/watch?v=ovT5AcJFaMQ') // Must be a YouTube video link
+        .setURL('https://www.youtube.com/watch?v=ovT5AcJFaMQ')
         .setState('☣ [King of Hackers] ☣')
         .setName('⚠◥◣_◢◤ "HACKER" ◥◣_◢◤⚠')
-        .setDetails('💢 ATOMIC-Tier Hacker 💢') // Corrected syntax
-        .setStartTimestamp(global.startTime);
-
-    try {
-        activity.setAssetsLargeImage('https://media.discordapp.net/external/svmVyE9opeT6hU2NHgmVT6M8N2xK9M76D0Z9NuaOOUk/https/i.imgur.com/uLmY6Lz.gif'); // Image link
-    } catch (error) {
-        console.error("Invalid URL for large image:", error.message);
-    }
-
-    activity.setAssetsLargeText('⚠ Classified as a national Threat ⚠') // Hover text for the large image
+        .setDetails('💢 ATOMIC-Tier Hacker 💢')
+        .setAssetsLargeImage('https://media.discordapp.net/external/svmVyE9opeT6hU2NHgmVT6M8N2xK9M76D0Z9NuaOOUk/https/i.imgur.com/uLmY6Lz.gif');
+        .setAssetsLargeText('⚠ Classified as a national Threat ⚠')
         .addButton('♠ "Illegal Video" ♠', 'https://www.youtube.com/watch?v=ovT5AcJFaMQ');
     
     client.user.setActivity(activity.toJSON());
